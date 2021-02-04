2262cac71294f9c8bd343c1ec2b509ecf2642e8d
mouarius <mouarius@gmail.com>
2021-02-04 01:44:51 +0100

    Login page begins

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import Card from './model/card'
+import { useState } from 'react'
+import Player from './model/player'
+import { teamColor } from './model/common'
+
+let _ = require('lodash')
+
+const Login = () => {
+  const [username, setUsername] = useState('')
+  const [color, setColor] = useState(teamColor.RED)
+  const [player, setPlayer] = useState(null)
+
+  const createPlayer = (e) => {
+    e.preventDefault()
+    const newPlayer = new Player(color, username)
+    setUsername('')
+    setPlayer(newPlayer)
+  }
+
+  return (
+    <div id="login-window">
+      <form onSubmit={createPlayer}>
+        <label for="username">username :</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <br />
+        <ul>
+          {_.map(teamColor, (element) => (
+            <li>
+              <label for={`${element}-radio`}>{element}</label>
+              <input
+                type="radio"
+                id={`${element}-radio`}
+                name="color"
+                value={element}
+                checked={element === color}
+                onChange={(e) => setColor(e.target.value)}
+                class="color-radio"
+              />
+            </li>
+          ))}
+        </ul>
+
+        <br />
+
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Login />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,28 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from '@testing-library/react'
+import App from './App'
+import Card from './model/card'
+import Deck from './model/deck'
+import { cardType, teamColor } from './model/common'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+  render(<App />)
+  const linkElement = screen.getByText(/learn react/i)
+  expect(linkElement).toBeInTheDocument()
+})
+
+describe('CARDS AND DECKS', () => {
+  test('a card can be created', () => {
+    const newCard = new Card(teamColor.ORANGE, cardType.FLOWER, 1)
+    expect(newCard).not.toBeNull()
+  })
+  test('a deck is initialized with the correct number of cards', () => {
+    const newDeck = new Deck(teamColor.BLUE)
+    expect(newDeck.cards).toHaveLength(4)
+  })
+  test('a card can be played', () => {
+    const newDeck = new Deck(teamColor.BLUE)
+    const someCard = newDeck.cards[0]
+    someCard.play()
+    expect(someCard['isInGame']).toBe(true)
+  })
+})
