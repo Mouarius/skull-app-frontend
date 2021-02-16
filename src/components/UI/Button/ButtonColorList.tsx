c96587098b98be30a0c9cd455f3c2acf46e5684d
mouarius <mouarius@gmail.com>
2021-02-16 06:34:23 +0100

    ButtonColorList Typed

---
+++ src/components/UI/Button/ButtonColorList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import ButtonColor from './ButtonColor';
+import _ from 'lodash';
+import { TeamColor } from '../../../util/types';
+
+interface ButtonColorListProps {
+  takenColors: TeamColor[];
+}
+
+const ButtonColorList: React.FC<ButtonColorListProps> = (props) => {
+  return (
+    <div className="ButtonColorList">
+      <h2 className="mb-2">Select your color :</h2>
+      <ul className="flex flex-row items-center justify-between w-full mb-8">
+        {_.map(Object.values(TeamColor), (color) => (
+          <ButtonColor
+            key={color}
+            color={color}
+            takenColors={props.takenColors}
+          />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ButtonColorList;
